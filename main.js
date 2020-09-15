555ea034be5283d2e03b2f17cb9c359c9f9b2e13
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-09-15 14:43:07 -0400

    Completed question 2.

--- main.js
+++ main.js
@@ -29,12 +29,41 @@ let users = [
     }
 ];
 
+// Definitely prefer implicit functions when possible, but doing both for practice
 // 1. Answer(Implicitly):
-// let fullNames = users.map((user) => `${user.firstName} ${user.lastName}`);
+let fullNamesImp = users.map((user) => `${user.firstName} ${user.lastName}`);
 
 // 1. Answer(Explicitly):
-let fullNames = users.map((user) => {
+let fullNamesExp = users.map((user) => {
     return `${user.firstName} ${user.lastName}`;
 });
 
-console.log(fullNames);
+console.log(fullNamesImp);
+console.log(fullNamesExp);
+
+// 2a. Create an array of objects where the value from the name key in each object in users is the 
+//     key in the new object index and the likes value is the value in the new object index.
+//  b. Calculate age based on the number of letters in the name multiplied by 10
+const users2 = [
+    { name: 'pan', likes: 'lasagne' },
+    { name: 'lena', likes: 'wine' },
+    { name: 'jill', likes: 'tacos' },
+    { name: 'conner', likes: 'turkey' }
+];
+
+let people = users2.map((user) => {
+    let age = user.name.length * 10;
+    return {[user.name]:user.likes, age : age};
+});
+
+console.log(people)
+
+// Should look like:
+// [
+//     { pan: 'lasagne', age: 30 },
+//     { lena: 'wine', age: 40 },
+//     { jill: 'tacos', age: 40 },
+//     { conner: 'turkey', age: 60 }
+// ]
+
+
